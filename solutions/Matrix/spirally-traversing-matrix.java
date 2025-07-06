af6f97e6d3003c3882266138f3568cf506c01e0f
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-07-06 18:31:59 +0530

    Create spirally-traversing-matrix.java

---
+++ solutions/Matrix/spirally-traversing-matrix.java
@@ -0,0 +1,42 @@
+/*
+Time Complexity : O(m * n)
+Space Complexity : O(1)
+
+linkedIn : www.linkedin.com/in/mukundasv03 
+Github : https://github.com/mukunda03
+*/
+
+class Solution {
+    public ArrayList<Integer> spirallyTraverse(int[][] mat) {
+        // code here
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int top =0,bottom = m-1, left =0, right = n-1;
+        while(top <= bottom && left <= right){
+            for(int i = left; i <= right;++i){
+                res.add(mat[top][i]);
+            }
+            top++;
+            for(int i = top; i<= bottom; ++i){
+                res.add(mat[i][right]);
+            }
+            right--;
+            if(top <= bottom){
+                for(int i = right; i >= left; --i){
+                    res.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right){
+                for(int i = bottom; i >= top; --i){
+                    res.add(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
