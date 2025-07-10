458d36d74242fcdb41bc0b9ed26425bab18cb589
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-07-10 21:45:23 +0530

    Create search-in-a-sorted-matrix.java

---
+++ solutions/Matrix/search-in-a-sorted-matrix.java
@@ -0,0 +1,33 @@
+/*
+Time Complexity : O(log(n*m))
+Space Complexity : O(1)  
+
+linkedIn : www.linkedin.com/in/mukundasv03 
+Github : https://github.com/mukunda03
+*/
+
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        int n = mat.length, m=mat[0].length;
+        
+        int lo= 0, hi = n*m-1;
+        while(lo <= hi) {
+            int mid = (lo+hi)/2;
+            
+            int row = mid/m;
+            int col = mid%m;
+            
+            if(mat[row][col] == x)
+            return true;
+            
+            if(mat[row][col] < x)
+            lo = mid +1;
+            
+            else
+            hi = mid -1;
+        }
+        return false;
+    }
+}
