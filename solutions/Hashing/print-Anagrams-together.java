7ee4735fe9cfd3325ed10ffcf076b6fefada0049
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-07-18 08:45:51 +0530

    Create print-Anagrams-together.java

---
+++ solutions/Hashing/print-Anagrams-together.java
@@ -0,0 +1,42 @@
+/*
+Time Complexity :O(n*k)
+Space Complexity : O(n*k)
+
+linkedIn : www.linkedin.com/in/mukundasv03 
+Github : https://github.com/mukunda03
+*/
+import java.util.*;
+
+class Solution {
+    static final int MAX_CHAR = 26;
+    
+    static String getHash(String s){
+        StringBuilder hash = new StringBuilder();
+        int[] freq = new int[MAX_CHAR];
+        
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        for(int i =0;i<MAX_CHAR;i++){
+            hash.append(freq[i]);
+            hash.append("$");
+        }
+        return hash.toString();
+    }
+    
+    public ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        // code here
+        ArrayList<ArrayList<String>> res = new ArrayList<>();
+        Map<String,Integer> mp = new HashMap<>();
+        
+        for(int i =0;i<arr.length;i++){
+            String key = getHash(arr[i]);
+            if(!mp.containsKey(key)){
+                mp.put(key,res.size());
+                res.add(new ArrayList<>());
+            }
+            res.get(mp.get(key)).add(arr[i]);
+        }
+        return res;
+    }
+}
