7e02f264dadf05550b82a01005d5b18e3aa386f0
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-09-12 20:07:48 +0530

    Create Find-median-in-a-stream.java

---
+++ solutions/Heap/Find-median-in-a-stream.java
@@ -0,0 +1,40 @@
+/*
+Time Complexity :  O(n*logn) 
+Space Complexity : O(n)
+
+linkedIn : www.linkedin.com/in/mukundasv03
+Github : https://github.com/mukunda03
+*/
+
+class Solution {
+    public ArrayList<Double> getMedian(int[] arr) {
+        // code here
+        PriorityQueue<Integer> leftMaxHeap = new PriorityQueue<>((a,b) -> b-a);
+        
+        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>();
+        
+        ArrayList<Double> res = new ArrayList<>();
+        
+        for(int i =0;i<arr.length; i++){
+            leftMaxHeap.add(arr[i]);
+            
+            int temp = leftMaxHeap.poll();
+            rightMinHeap.add(temp);
+            
+            if(rightMinHeap.size() > leftMaxHeap.size()) {
+                temp = rightMinHeap.poll();
+                leftMaxHeap.add(temp);
+            }
+            
+            double median;
+            
+            if(leftMaxHeap.size() != rightMinHeap.size())
+            median = leftMaxHeap.peek();
+            else
+            median = (leftMaxHeap.peek() + rightMinHeap.peek()) /2.0;
+            
+            res.add(median);
+        }
+        return res;
+    }
+}
