59d17831b13d594d743d039e3dbe6c9247e596d9
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-09-23 19:30:54 +0530

    Create Longest-Bounded-Difference-Subarray.java

---
+++ solutions/Queue and Deque/Longest-Bounded-Difference-Subarray.java
@@ -0,0 +1,54 @@
+/*
+Time Complexity :  O(n) 
+Space Complexity : O(n)
+
+linkedIn : www.linkedin.com/in/mukundasv03
+Github : https://github.com/mukunda03
+*/
+
+
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> longestSubarray(int[] arr, int x) {
+        // code here
+        Deque<Integer> minQueue = new LinkedList<>();
+        Deque<Integer> maxQueue = new LinkedList<>();
+        
+        int n = arr.length, start = 0, end =0;
+        int resStart = 0, resEnd = 0;
+        
+        while(end < n){
+            
+            while(!minQueue.isEmpty() && arr[minQueue.peekLast()] > arr[end])
+            minQueue.pollLast();
+            
+            while(!maxQueue.isEmpty() && arr[maxQueue.peekLast()] < arr[end])
+            maxQueue.pollLast();
+            
+            minQueue.addLast(end);
+            maxQueue.addLast(end);
+            
+            while(arr[maxQueue.peekFirst()] - arr[minQueue.peekFirst()] > x){
+                if(start == minQueue.peekFirst())
+                minQueue.pollFirst();
+                
+                if(start == maxQueue.peekFirst())
+                maxQueue.pollFirst();
+                start += 1;
+            }
+            
+            if(end - start > resEnd - resStart){
+                resStart = start;
+                resEnd =end;
+            }
+            end += 1;
+        }
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = resStart; i<= resEnd;i++)
+        res.add(arr[i]);
+        
+        return res;
+    }
+}
