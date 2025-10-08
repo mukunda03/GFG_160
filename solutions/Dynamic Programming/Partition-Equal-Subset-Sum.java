63f97cce176a55a800a84b06ee2a1b852c41eac8
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-10-08 07:14:20 +0530

    Create Partition-Equal-Subset-Sum.java

---
+++ solutions/Dynamic Programming/Partition-Equal-Subset-Sum.java
@@ -0,0 +1,41 @@
+/*
+Time Complexity :  O(sum*n)
+Space Complexity : O(sum)
+
+linkedIn : www.linkedin.com/in/mukundasv03
+Github : https://github.com/mukunda03
+*/
+
+class Solution {
+    static boolean equalPartition(int arr[]) {
+        // code here
+        int sum = 0;
+        for(int num : arr){
+            sum += num;
+        }
+        
+        if(sum % 2 != 0)
+        return false;
+        
+        sum = sum/2;
+        
+        int n = arr.length;
+        boolean[] prev = new boolean[sum+1];
+        boolean[] curr = new boolean[sum+1];
+        
+        prev[0] = true;
+        
+        for(int i =1; i <= n ;i++){
+            for(int j = 0; j <= sum; j++){
+                if(j < arr[i-1])
+                curr[j] = prev[j];
+                else
+                curr[j] = (prev[j] || prev[j-arr[i-1]]);
+            }
+            for(int j =0; j<= sum; j++){
+                prev[j] = curr[j];
+            }
+        }
+        return prev[sum];
+    }
+}
