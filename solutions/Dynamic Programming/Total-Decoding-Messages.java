c89e4c576c24cee15c7bff65f97a53ba7701216a
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-10-13 22:05:14 +0530

    Create Total-Decoding-Messages.java

---
+++ solutions/Dynamic Programming/Total-Decoding-Messages.java
@@ -0,0 +1,39 @@
+/*
+Time Complexity :  O(n)
+Space Complexity : O(1)
+
+linkedIn : www.linkedin.com/in/mukundasv03
+Github : https://github.com/mukunda03
+*/
+
+// User function Template for Java
+class Solution {
+    public int countWays(String digits) {
+        // code here
+        int n = digits.length();
+        
+        if(n==0 || digits.charAt(0) == '0'){
+            return 0;
+        }
+        
+        int prev1 = 1, prev2 = 0;
+        
+        for(int i =1; i <= n;++i){
+            int current = 0;
+            
+            if(digits.charAt(i-1) != '0'){
+                current += prev1;
+            }
+            
+            if( i > 1){
+                int twoDigit = (digits.charAt(i-2) - '0')*10 + (digits.charAt(i-1) - '0');
+                if(twoDigit >= 10 && twoDigit <=26){
+                    current += prev2;
+                }
+            }
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
