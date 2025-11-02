d869562007bda30e45384aa6c7e865d6a5be5114
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-11-02 09:30:47 +0530

    Update Dijkstra-Algorithm.java

--- solutions/Graph/Dijkstra-Algorithm.java
+++ solutions/Graph/Dijkstra-Algorithm.java
@@ -1,70 +1,46 @@
 /*
-Time Complexity :  O(E*logV)
-Space Complexity : O(V)
+Time Complexity :  O((V+E) log V)
+Space Complexity : O(V+E)
 
 linkedIn : www.linkedin.com/in/mukundasv03
 Github : https://github.com/mukunda03
 */
 
 class Solution {
-    
-    static ArrayList<ArrayList<ArrayList<Integer>>> constructAdj(int[][] edges, int V){
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
-        
-        for(int i =0; i < V;i++){
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        // code here
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i =0; i < V; i++){
             adj.add(new ArrayList<>());
         }
         
-        for(int [] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
-            
-            ArrayList<Integer> e1 = new ArrayList<>();
-            e1.add(v);
-            e1.add(wt);
-            adj.get(u).add(e1);
-            
-            ArrayList<Integer> e2 = new ArrayList<>();
-            e2.add(u);
-            e2.add(wt);
-            adj.get(v).add(e2);
+        for(int[] e : edges){
+            int u = e[0], v = e[1], w = e[2];
+            adj.get(u).add(new int[]{v,w});
         }
-        return adj;
-    }
-    
-    public int[] dijkstra(int V, int[][] edges, int src) {
-        // code here
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = constructAdj(edges, V);
-        
-        PriorityQueue<ArrayList<Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.get(0)));
         
         int[] dist = new int[V];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        
         dist[src] = 0;
-        ArrayList<Integer> start = new ArrayList<>();
         
-        start.add(0);
-        start.add(src);
-        pq.offer(start);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        pq.offer(new int[]{0,src});
         
         while(!pq.isEmpty()){
-            ArrayList<Integer> curr = pq.poll();
-            int d = curr.get(0);
-            int u = curr.get(1);
+            int[] curr = pq.poll();
+            int d = curr[0];
+            int u = curr[1];
             
-            for(ArrayList<Integer> neighbor : adj.get(u)){
-                int v = neighbor.get(0);
-                int weight = neighbor.get(1);
+            if(d > dist[u]) continue;
+            
+            for(int[] edge : adj.get(u)){
+                int v = edge[0];
+                int w = edge[1];
                 
-                if(dist[v] > dist[u] + weight){
-                    dist[v] = dist[u] + weight;
-                    
-                    ArrayList<Integer> temp  = new ArrayList<>();
-                    temp.add(dist[v]);
-                    temp.add(v);
-                    pq.offer(temp);
+                if(dist[v] > dist[u] + w){
+                    dist[v] = dist[u] + w;
+                    pq.offer(new int[]{dist[v],v});
                 }
             }
         }
