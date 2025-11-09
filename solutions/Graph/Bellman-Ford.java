991a696f8a404709e8a79e0dfc2712600f8ca9e6
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-11-09 13:58:37 +0530

    Create Bellman-Ford.java

---
+++ solutions/Graph/Bellman-Ford.java
@@ -0,0 +1,32 @@
+/*
+Time Complexity :  O(V*E)
+Space Complexity : O(V)
+
+linkedIn : www.linkedin.com/in/mukundasv03
+Github : https://github.com/mukunda03
+*/
+// User function Template for Java
+
+class Solution {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        int[] dist = new int[V];
+        Arrays.fill(dist,(int)1e8);
+        dist[src] = 0;
+        
+        for(int i =0; i<V; i++){
+            for(int[] edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
+                    if(i == V-1)
+                    return new int[]{-1};
+                    
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        return dist;
+    }
+}
