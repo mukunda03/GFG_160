ac0a6d8fee06519277a4cfc204dd002cdc6ebe1a
Mukunda <118037007+mukunda03@users.noreply.github.com>
2025-11-10 20:02:46 +0530

    Create Floyd-Warshall.java

---
+++ solutions/Graph/Floyd-Warshall.java
@@ -0,0 +1,25 @@
+/*
+Time Complexity :  O(V^3)
+Space Complexity : O(1)
+
+linkedIn : www.linkedin.com/in/mukundasv03
+Github : https://github.com/mukunda03
+*/
+
+// User function template for JAVA
+
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        // Code here
+        int V = dist.length;
+        
+        for(int k =0; k < V; k++){
+            for(int i = 0; i < V; i++){
+                for(int j =0; j < V; j++){
+                    if(dist[i][k] != 1e8 && dist[k][j] != 1e8)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}
